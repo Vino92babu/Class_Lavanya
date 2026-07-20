c2a9f7b191fd01984cee48cb4da3b2e7575726ec
vinoth <vino92babu@gmail.com>
2026-07-20 13:44:52 +0530

    print_sing_double_quotes

--- print.py
+++ print.py
@@ -1,2 +1,13 @@
-print("Hi Vinoth")
-print("afc")
+# syntax --> print(object,seperator,end)
+# object syntax --> single quotes ' ' / " "
+# single qu --> 
+# print('welcome')
+# print("welcome")
+# o/p --> you're here to learn python
+# print("you're here to learn python")
+
+# print("you're here to learn python")
+# # o/p --> "you are here to learn python"
+# print('"you are here to learn python"')
+
+# o/p --> "you're here to learn python"
