654a6738c29c525608b0d0b090bab137e7392043
vinoth <vino92babu@gmail.com>
2026-07-21 13:13:53 +0530

    Print_class_completed

--- print.py
+++ print.py
@@ -11,3 +11,66 @@
 # print('"you are here to learn python"')
 
 # o/p --> "you're here to learn python"
+# Escapce sequence (the blackslash '\' --> Character)'
+
+# print('"you\'re here to learn python"')
+# print("\"you're here to learn python"")
+
+# i/p --> "hi how are you"
+
+# print("hi how are you")
+
+# new line --> ("\n")
+
+'''
+hi
+how 
+are
+you
+'''
+# print("hi\nhow\nare\nyou")
+
+'''
+o/p
+Name    :   Vinoth
+Gender  :   Male
+'''
+
+# print("Name : Vinoth \n Gender : Male")
+
+# print("Name\t:\tVinoth \nGender\t:\tMale")
+
+# default sep(,)
+# keyword (sep =' ')
+
+# happyworld
+
+# print("happy","world")
+
+# happy_world
+# happy,world
+
+# print("happy","world",sep="@")
+
+# happy @ world
+
+# print("happy",,,"world",sep="@")
+
+# print("happy "," world",sep="@")
+
+# End sequence
+'''
+welcome 
+To
+Python
+o/p--> welcome To Python
+'''
+# print("welcome",end=" ")
+# print("To",end=" ")
+# print("python",end="")
+
+'''
+Welcome Vinoth
+    * Have a
+        * Nice Day.
+'''
