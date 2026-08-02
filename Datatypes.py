4223ed790c3ac17473e316567918659d8d2a09da
vinoth <vino92babu@gmail.com>
2026-08-02 21:02:39 +0530

    tuple

--- Datatypes.py
+++ Datatypes.py
@@ -247,9 +247,7 @@ A list containg items seperated by commas and enclosed with sq brackets ([])'''
 
 # print(A+B)
 
-# update  list
-
-# "append()"
+# updating  list
 
 # Num = [10,20,30,40,50,60]
 # Num[2] = 90 --> [10,20,90,40,50,60]
@@ -260,3 +258,137 @@ A list containg items seperated by commas and enclosed with sq brackets ([])'''
 # [1,2,3,90,50,60]
 
 # [1,2,90,3,4,60]
+
+
+# Delete list elements
+# Num = [10,20,30,40,50,60]
+# del Num[3]
+# del Num[3:5]
+# A = [10,"Vino",8.5,"ECE"]
+
+
+# del A[1:3+1]
+
+
+# print(A)
+
+
+
+# Basic list operations: 
+# A = [1,2,3,90,50,60]
+# l_list = len(A)
+# print(l_list)
+# print(len(A))
+
+# Concatenation ("+" --> for list)
+
+# A = [10,30]
+# B = [20,40]
+# print(A+B)
+
+# repetition ("*")
+
+# A =[10,20,30]
+
+
+# print(A*2)
+
+# Membership Operator --> ("in") --> true/false
+
+# students = ["Vinoth","Babu","Raju","Lavanya"]
+# print("Lavanya" in students)
+
+# Build- in functions /Methods: 
+'''
+len() --> this method returns the number of elements in list --> len(list)
+max() --> this method returns the elements from the list with maximum value. --> max(list)
+min() --> this method returns the elements from the list with minimum value. --> min(list)
+list() --> this method takes sequence type and conver them to list. This is used to convert a given tuple to list. --> list(seq)
+append() --> This method appends a passed object ito existing list
+count() --> This method returns count of how many times,that obj occurs in list
+extend() --> This method appends the contents of seq to list : Note: For numeric number extend will not be perform 
+index() --> This method returns the lowest index in list that obj appers. 
+insert() --> To add the elements in specific index.
+pop() --> This methos removes and teturn last object the list / used to delete the elements in the last, if we doesn't give the index value.
+remove() --> it is used to delete the elements when we does not able to find the index value. 
+reverse() --> To used to print the element in list in reverse order. 
+sort() --> it is used to keep elements in accending order (0-1). And for string it will be arrange in alphabetic order.
+'''
+# A = [1,2,3,90,50,60]
+
+# print(max(A))
+# print(min(A))
+
+# list_ITEM = ["Green","Black","Yellow","Black"]
+# list_ITEM.append("blue")
+# print(list_ITEM)
+
+# print(list_ITEM.count("Black"))
+
+# list_ITEM = ["Green","Black","Yellow","Black"]
+# list_ITEM.extend("Red")
+# list_ITEM.extend(2)
+# print(list_ITEM)
+# B = list_ITEM.insert(2,"red")
+# print(B)
+
+# without index 
+# A = [1,2,3,4]
+# A.pop()
+# print(A)
+
+# with index 
+# A.pop(1)
+# print(A)
+
+# A = [1,2,3,4]
+# # A.remove(1)
+# # print(A)
+# B = A.reverse()
+# print(B)
+
+
+# A = [50,60,1,8,0,25,41,65,56,28,16] 
+# A.sort()
+# print(max(A))
+# print(A)
+
+# tuple
+'''
+A tuple is a seq of immutable python obj. 
+the main difference between the tupple and list is that the tuple cannot be changed unlike list. 
+Tuple use parantheses"()" , where as list use square brackects "[]"
+
+Note: Like string indices, tuple indices also start at 0 and they can be sliced , concatenated and so on. 
+
+'''
+# l = [1,2,3,4,5]
+# l1 = []
+# t1 = ()
+# t = (1,2,3,4,5)
+# print(type(l))
+# print(type(t))
+# print(type(l1))
+# print(type(t1))
+# Accessing value in tuples: 
+# tuple1 = (10,"rise",2.5,"ece")
+# print(tuple1[2])
+
+# print("tuple [2 is :]" , tuple1[2])
+
+# updating Tuples : Tuples are immutable, which means you cannot update or change the values of tuple elements. 
+'''
+Delete tuple elements: 
+Removing individual tuple elements is not possible . 
+But To explicitly we can revome the an entire tuple, Just using the "del" statement
+'''
+
+# abc = (1,2,34)
+# bc = [1,2,34]
+
+# del abc [1]
+
+# print(abc)
+
+# del bc [2]
+# print(bc)
