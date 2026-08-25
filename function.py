070f13ac2c347ed14f87729c7aaf6712d40abbb4
vinoth <vino92babu@gmail.com>
2026-08-25 07:59:52 +0530

    function completed

--- function.py
+++ function.py
@@ -2,7 +2,9 @@
 # B = 48 
 # C= A+B 
 # print(C)
-
+def add(a,b):
+    c = a+b
+    print(c)
 
 # def add():
 #     A = 3
@@ -20,3 +22,39 @@ Return, with arguments
 '''
 
 # No return, without arguments
+def Add():
+    a = 3
+    b = 6
+    c = a+b
+    print(c)
+
+# add()
+
+def url():
+    base_url = "www.google.com"
+    print(base_url)
+    return(base_url)
+
+
+# No Return, with arguments
+
+# Add(74,96)
+
+# Return, without arguments
+
+def sub():
+    a = 85
+    b = 41
+    return a - b
+result = sub()
+print(result)
+
+
+# Return, with arguments
+
+def div(a,b):
+    return a/b
+div()
+
+
+
